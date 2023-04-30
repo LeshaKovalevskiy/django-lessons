0e5ea434d9f5aa500971ef1b9e674dcb9cef5ba9
LeshaKov <lelya.kovalevskiy02@mail.ru>
2023-04-30 22:49:03 +0300

    Displaying the form in the template, saving the submitted form to the database

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -3,7 +3,11 @@ from .models import *
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label="Название",
+                            widget=forms.TimeInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label="Текст", required=False,
+                              widget=forms.Textarea(attrs={"class": "form-control", "rows": 5}))
+    is_published = forms.BooleanField(label="Опубликовано?", initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категория",
+                                      empty_label="Выберите категорию",
+                                      widget=forms.Select(attrs={"class": "form-control"}))
